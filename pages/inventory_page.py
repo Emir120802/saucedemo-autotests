61613036bd266b382b2b5bd90a4b816d33a05b19
Emir120802 <kemir4580@gmail.com>
2025-12-24 15:05:04 +0600

    Fix: stable locator for T-Shirt in parameterized test

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -4,16 +4,15 @@ class InventoryPage(BasePage):
     # Локаторы
     CART_BADGE = ".shopping_cart_badge"
     CART_LINK = ".shopping_cart_link"
-    # Добавляем локатор для рюкзака обратно
     ADD_TO_CART_SAUCE_BACKPACK = "#add-to-cart-sauce-labs-backpack"
 
     def add_backpack_to_cart(self):
-        """Метод для обратной совместимости со старыми тестами"""
-        self.page.wait_for_selector(self.ADD_TO_CART_SAUCE_BACKPACK, state="visible")
+        """Метод для обратной совместимости: добавляет рюкзак в корзину"""
+        self.page.wait_for_selector(self.ADD_TO_CART_SAUCE_BACKPACK, state="visible", timeout=5000)
         self.page.click(self.ADD_TO_CART_SAUCE_BACKPACK)
 
     def get_cart_count(self):
-        """Возвращает количество товаров или '0', если корзина пуста"""
+        """Возвращает число товаров в корзине или '0', если счетчика нет"""
         try:
             self.page.wait_for_selector(self.CART_BADGE, state="visible", timeout=2000)
             return self.page.inner_text(self.CART_BADGE)
@@ -24,7 +23,7 @@ class InventoryPage(BasePage):
         self.page.click(self.CART_LINK)
 
     def add_item_to_cart_by_name(self, item_name):
-        """Универсальный метод для добавления любого товара по имени"""
+        """Универсальный метод добавления товара по его названию"""
         clean_name = item_name.lower().replace(" ", "-")
         selector = f"button[id='add-to-cart-{clean_name}']"
         
